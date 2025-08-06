c90b438dbfc9fd91d28795892834f0ad44ecae95
H3llynx <helene.teychene@gmail.com>
2025-08-06 12:23:51 +0200

    Updates

---
+++ assets/SpeedIcon-BWTBWk8h.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,a as r,o as s}from"./index-B7PHOr9N.js";const n={},c={xmlns:"http://www.w3.org/2000/svg",height:"24px",viewBox:"0 -960 960 960",width:"24px",fill:"currentColor"};function a(l,e){return s(),t("svg",c,e[0]||(e[0]=[r("path",{d:"m280-80 160-300-320-40 480-460h80L520-580l320 40L360-80h-80Z"},null,-1)]))}const _=o(n,[["render",a]]);export{_ as S};
